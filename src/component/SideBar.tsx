a6e100f84ad441cba4a023ca6dbab268d80af75e
KunalatGH <codesnippet.42@gmail.com>
2025-09-18 23:46:55 +0530

    commit

--- src/component/SideBar.tsx
+++ src/component/SideBar.tsx
@@ -8,22 +8,22 @@ const SideBar = () => {
             <h2>Hot Deals</h2>
         </div>
         <div id="categories">
-            <div class="cat_head">
+            <div className="cat_head">
             <h2>Nike</h2><span>2</span>
             </div>
-            <div class="cat_head">
+            <div className="cat_head">
             <h2>Adidas</h2><span>2</span>
             </div>
-            <div class="cat_head">
+            <div className="cat_head">
             <h2>Airmax</h2><span>2</span>
             </div>
-            <div class="cat_head">
+            <div className="cat_head">
             <h2>Airmax</h2><span>2</span>
             </div>
-            <div class="cat_head">
+            <div className="cat_head">
             <h2>Van</h2><span>2</span>
             </div>
-            <div class="cat_head">
+            <div className="cat_head">
             <h2>All Stars</h2><span>2</span>
             </div>
         </div>
